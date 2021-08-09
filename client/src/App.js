4f4307ae7612928289603f0d61da8333cb0c7dd6
Rajan Bastakoti <rbastakoti@student.ysu.edu>
2021-08-09 14:24:54 -0400

    First Commit: React Design with Bootstrap features

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './css/bootstrap.css';
 
 
-function Header(){
+function Header(props){
     return(
-        <header className={App} style={{background: "powderblue"}}>
-            <h1>Friend Circle</h1>
+        <header className="shadow rounded" style={{background: "powderblue"}}>
+            <h1 style={{display:"block"}}>{props.name}</h1>
         </header>
     );
 }
@@ -18,23 +18,37 @@ function Main(){
     );
 }
 
-function Footer(){
+function Footer(props){
     return(
         <footer>
-            <p>Copyright@ 2020</p>
+            <p>Copyright@ {props.year}</p><br/>
+            <div className = "w-25 p-3" style={{float:"left"}} >
+                <div className="list-group" align={"left"} >
+                    <ul className="list-group">
+                    <p className="font-weight-bold">Web Features</p>
+                    {props.users.map((user) =><li className="list-group-item">{user}</li>)}
+                    </ul>
+                </div>
+            </div>
         </footer>
 
     );
 }
 
+const users = [
+    "Create a Profile",
+    "Search People",
+    "Get Connected"
+];
+
 
 function App() {
     return (
 
-        <div className={"container"} align="center">
-            <Header />
+        <div className="container" align="center">
+            <Header name = "Friend Circle"/>
             <Main />
-            <Footer />
+            <Footer year = {new Date().getFullYear()} users = {users}/>
 
         </div>
 
